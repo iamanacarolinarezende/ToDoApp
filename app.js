82f2624ed0cd29a7ad3f1ace43c78a26c974b0a8
Ana Carolina Rezende <anacarolina@bdhcollective.com>
2025-03-04 12:49:59 -0500

    MongoDB Atlas connection
    Connection with mongodb and bug fixes

--- app.js
+++ app.js
@@ -5,19 +5,36 @@ const taskRoutes = require('./routes/tasks');
 
 const app = express();
  
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/todo-list', {
+//MongoDB Atlas
+const mongoURI = "mongodb+srv://cre8webinc:Fx8JRTWnSouhgJ5q@cluster0.jqprt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//MongoDB Atlas Connection
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Conectado ao MongoDB'))
-.catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+.then(() => console.log('Conectado ao MongoDB Atlas'))
+.catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
+
+// Configurações do Express
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+//// MongoDB Connection
+//mongoose.connect('mongodb://localhost:27017/todo-list', {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+//})
+//.then(() => console.log('Conectado ao MongoDB'))
+//.catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
 // Express Settings
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static('public')); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/', taskRoutes);
--- models/Task.js
+++ models/Task.js
@@ -11,7 +11,7 @@ const taskSchema = new mongoose.Schema({
     },
     priority: {
         type: String,
-        enum: ['Hight', 'medium', 'low'],
+        enum: ['hight', 'medium', 'low'],
         default: 'medium',
     },
 });
