6e2eaf1a1ede7927f860d4160f53e92d3810c88d
Ana Carolina Rezende <72672907+iamanacarolinarezende@users.noreply.github.com>
2025-03-05 11:22:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const taskRoutes = require('./routes/tasks');
+const taskRoutes = require('./routes/Tasks');
 
 const app = express();
  
